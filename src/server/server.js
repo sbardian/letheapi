0ec4f9de27d1f2053dc0ee88fd923eda127a5708
Brian Andrews <sbardian@gmail.com>
2018-04-22 22:34:24 -0600

    Base server

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 
-const app = express();
-
-app.listen(9999, () => {
-  console.log('Server is up and listening on port 9999');
-});
+export default () => {
+  const server = express();
+  return server;
+};
---
+++ src/server/server.test.js
@@ -0,0 +1,5 @@
+import { default as createServer } from './server';
+
+test('Should return a server', () => {
+  expect(createServer()).toBeTruthy();
+});
