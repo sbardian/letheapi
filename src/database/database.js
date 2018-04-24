5b4e9c33a1cc11b9d77625aa269f5ce361d7fe0c
Brian Andrews <sbardian@gmail.com>
2018-04-24 10:53:04 -0600

    Fix import and remove useMongoClient

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import Item from './model';
-import shoppingItems from './mocks';
+import { shoppingItems } from './mocks';
 
 export default () => {
   mongoose.promise = global.Promise;
   const db_url = 'mongodb://localhost/shoppinglist';
-  mongoose.connect(db_url, { useMongoClient: true });
+  mongoose.connect(db_url);
   const db = mongoose.connection;
   Item.insertMany(shoppingItems);
   return db;
