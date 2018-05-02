1ab4e936be3c36ece8783a63f205361dea6576b6
Brian Andrews <sbardian@gmail.com>
2018-05-01 21:57:32 -0600

    Add get lists filter for title

--- src/graphql/businessLogic/authorizeGetLists.js
+++ src/graphql/businessLogic/authorizeGetLists.js
@@ -3,7 +3,20 @@ import jwt from 'jsonwebtoken';
 import { returnLists } from '../../database/utils';
 import { JWT_SECRET } from '../../config/config';
 
-export const authorizeGetLists = async (user, userId, limit, List, User) =>
+export const authorizeGetLists = async (
+  user,
+  userId,
+  limit,
+  title_contains,
+  List,
+  User,
+) => {
   // TODO: implement user check.
   // (await Item.find({username: user}).limit(limit)).map(returnItems);
-  (await List.find({ owner: userId }).limit(limit)).map(returnLists);
+  return (await List.find({
+    owner: userId,
+    ...(title_contains && {
+      title: { $regex: `${title_contains}`, $options: 'i' },
+    }),
+  }).limit(limit)).map(returnLists);
+};
--- src/graphql/resolvers/Queries/getLists.js
+++ src/graphql/resolvers/Queries/getLists.js
@@ -2,6 +2,6 @@ import { authorizeGetLists } from '../../businessLogic';
 
 export const getLists = (
   root,
-  { userId, limit },
+  { userId, limit, title_contains },
   { models: { List, User }, user },
-) => authorizeGetLists(user, userId, limit, List, User);
+) => authorizeGetLists(user, userId, limit, title_contains, List, User);
