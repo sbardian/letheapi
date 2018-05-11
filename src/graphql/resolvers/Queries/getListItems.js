07060cc4bc45bc713910e3780a836622d50e8ac1
Brian Andrews <sbardian@gmail.com>
2018-05-11 13:59:22 -0600

    Remove old code

--- src/graphql/resolvers/Queries/getListItems.js
+++ src/graphql/resolvers/Queries/getListItems.js
@@ -4,9 +4,4 @@ export const getListItems = async (
   { id },
   { limit = 500 },
   { models: { Item } },
-) =>
-  // (await Item.find({})
-  //   .where('list')
-  //   .equals(id)
-  //   .limit(limit)).map(returnItems);
-  (await Item.find({ list: id }).limit(limit)).map(returnItems);
+) => (await Item.find({ list: id }).limit(limit)).map(returnItems);
