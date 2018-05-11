5fb51bd88c1bc606a542ecf39ace713db03043c8
Brian Andrews <sbardian@gmail.com>
2018-05-11 11:09:25 -0600

    Remove isAdmin arg, pulling from user object

--- src/graphql/resolvers/Queries/getMyInfo.js
+++ src/graphql/resolvers/Queries/getMyInfo.js
@@ -1,7 +1,4 @@
 import { returnUsers } from '../../../database/utils';
 
-export const getMyInfo = async (
-  root,
-  args,
-  { models: { User }, user, isAdmin },
-) => returnUsers(await User.findById(user.id));
+export const getMyInfo = async (root, args, { models: { User }, user }) =>
+  returnUsers(await User.findById(user.id));
