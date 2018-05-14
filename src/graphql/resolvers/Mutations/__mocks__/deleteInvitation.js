3fb970e9ea666940f4ab8987c2b71c25a4f67d1b
Brian Andrews <sbardian@gmail.com>
2018-05-14 09:15:34 -0600

    Add test for declineInvitation

---
+++ src/graphql/resolvers/Mutations/__mocks__/deleteInvitation.js
@@ -0,0 +1,1 @@
+export const deleteInvitation = jest.fn();
---
+++ src/graphql/resolvers/Mutations/declineInvitation.test.js
@@ -0,0 +1,29 @@
+import { declineInvitation } from './declineInvitation';
+import mockInvitation from '../../../database/models/Invitation';
+import mockList from '../../../database/models/List';
+import * as mockDeleteInvitation from './deleteInvitation';
+
+jest.mock('../../../database/models/Invitation');
+jest.mock('../../../database/models/List');
+jest.mock('./deleteInvitation');
+
+describe('declineInvitation test', () => {
+  it('deleteInvitation should be called once', async () => {
+    mockInvitation.findById.mockImplementationOnce(() => 'someInvitationId');
+    mockList.findById.mockImplementationOnce(() => 'someListId');
+    mockDeleteInvitation.deleteInvitation.mockImplementationOnce(() => true);
+    const spyOnDeleteInvitation = jest.spyOn(
+      mockDeleteInvitation,
+      'deleteInvitation',
+    );
+    await declineInvitation(
+      'root',
+      { invitationId: 'invitationId' },
+      {
+        models: { Invitation: mockInvitation, List: mockList },
+        user: { id: 'someUserId' },
+      },
+    );
+    expect(spyOnDeleteInvitation).toHaveBeenCalledTimes(1);
+  });
+});
