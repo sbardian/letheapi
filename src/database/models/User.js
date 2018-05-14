354dd20d94847ca40cc2254e2fbf14cb31659410
Brian Andrews <sbardian@gmail.com>
2018-05-13 23:03:35 -0600

    Remove old code from User model

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,11 +1,4 @@
-/* eslint no-use-before-define: 0 */
-/* eslint func-names: 0 */
-/* eslint prefer-arrow-callback: 0 */
-/* eslint consistent-return: 0 */
-/* eslint no-shadow: 0 */
-
 import mongoose from 'mongoose';
-// import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -27,37 +20,6 @@ const UserSchema = new mongoose.Schema({
   lists: [String],
   isAdmin: Boolean,
 });
-
-// // authenticate input against database
-// UserSchema.statics.authenticate = (email, password, callback) => {
-//   User.findOne({ email }).exec((err, user) => {
-//     if (err) {
-//       return callback(err);
-//     } else if (!user) {
-//       const error = new Error('User not found.');
-//       error.status = 401;
-//       return callback(err);
-//     }
-//     bcrypt.compare(password, user.password, (err, result) => {
-//       if (result === true) {
-//         return callback(null, user);
-//       }
-//       return callback();
-//     });
-//   });
-// };
-
-// // hashing a password before storing it to the database
-// UserSchema.pre('save', function(next) {
-//   const user = this;
-//   bcrypt.hash(user.password, 10, function(err, hash) {
-//     if (err) {
-//       return next(err);
-//     }
-//     user.password = hash;
-//     next();
-//   });
-// });
 
 const User = mongoose.model('User', UserSchema);
 export default User;
