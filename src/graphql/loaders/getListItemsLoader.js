bc3ca25cec5841012470e8b46a9f47126deb5ac8
Brian Andrews <sbardian@gmail.com>
2018-05-15 10:04:56 -0600

    Remove cacheKeyFn

--- src/graphql/loaders/getListItemsLoader.js
+++ src/graphql/loaders/getListItemsLoader.js
@@ -2,14 +2,10 @@ import DataLoader from 'dataloader';
 import { returnItems } from '../../database/utils';
 
 export const getListItemsLoader = ({ Item }) =>
-  new DataLoader(
-    async listIds => {
-      const items = await Item.find({ list: { $in: listIds } });
-      let groupedArray = [];
-      listIds.forEach(id =>
-        groupedArray.push(items.filter(item => item.list === id)),
-      );
-      return groupedArray;
-    },
-    { cacheKeyFn: item => item },
-  );
+  new DataLoader(async listIds => {
+    const items = await Item.find({ list: { $in: listIds } });
+    return listIds.reduce(
+      (newArray, id) => [...newArray, items.filter(item => item.list === id)],
+      [],
+    );
+  });
