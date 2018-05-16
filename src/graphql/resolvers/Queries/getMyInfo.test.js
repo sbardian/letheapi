abd8747807a69db6b5fadaf649b4b3a6742e998c
Brian Andrews <sbardian@gmail.com>
2018-05-16 10:37:47 -0600

    Update getMyInfo tests

--- src/graphql/resolvers/Queries/getMyInfo.test.js
+++ src/graphql/resolvers/Queries/getMyInfo.test.js
@@ -1,26 +1,15 @@
 import { getMyInfo } from './';
-import mockUser from '../../../database/models/User';
+import mockGetMyInfoLoader from '../../loaders';
 
-jest.mock('../../../database/models/User');
+jest.mock('../../loaders');
 
 describe('getMyInfo tests', () => {
   it('Returns a user object', async () => {
-    mockUser.findById.mockImplementationOnce(() => ({
-      id: 'bobsId',
-      username: 'bob',
-      email: 'bob@bob.com',
-    }));
-    expect(
-      await getMyInfo('root', 'args', {
-        models: { User: mockUser },
-        user: { id: 'someUserId' },
-      }),
-    ).toEqual(
-      expect.objectContaining({
-        id: 'bobsId',
-        username: 'bob',
-        email: 'bob@bob.com',
-      }),
-    );
+    mockGetMyInfoLoader.load.mockImplementationOnce(() => true);
+    await getMyInfo('root', 'args', {
+      loaders: { getMyInfoLoader: mockGetMyInfoLoader },
+      user: { id: 'someUserId' },
+    }),
+      expect(mockGetMyInfoLoader.load).toHaveBeenCalledTimes(1);
   });
 });
