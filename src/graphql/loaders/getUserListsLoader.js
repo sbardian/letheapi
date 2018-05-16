87b4b85027c759a9a1bec98fad90f92bfb2b0607
Brian Andrews <sbardian@gmail.com>
2018-05-15 21:50:56 -0600

    Remove old comment

--- src/graphql/loaders/getUserListsLoader.js
+++ src/graphql/loaders/getUserListsLoader.js
@@ -3,7 +3,6 @@ import DataLoader from 'dataloader';
 export const getUserListsLoader = ({ List }) =>
   new DataLoader(async userIds => {
     const lists = await List.find({ users: { $in: userIds } });
-    console.log('found lists = ', lists);
     return userIds.reduce(
       (newArray, id) => [
         ...newArray,
