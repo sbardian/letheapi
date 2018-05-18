a72fba3444428c094292dd4a3a10fef707760752
Brian Andrews <sbardian@gmail.com>
2018-05-18 10:49:45 -0600

    Fix lists match in loader

--- src/graphql/loaders/getListsLoader.js
+++ src/graphql/loaders/getListsLoader.js
@@ -1,10 +1,10 @@
 import DataLoader from 'dataloader';
 
-export const getListsLoader = ({ List, User }) =>
+export const getListsLoader = ({ List }) =>
   new DataLoader(async listIds => {
     const lists = List.find({ _id: { $in: listIds } });
     return listIds.reduce((newArray, id) => [
       ...newArray,
-      lists.filter(list => list.id === listIds),
+      lists.filter(list => list.id === id),
     ]);
   });
