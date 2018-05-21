f9d22db7b0b8e2f2b2c5fea7f7edce3e3ffbd52a
Brian Andrews <sbardian@gmail.com>
2018-05-20 22:36:29 -0600

    Add returnUsers to getMyInfo

--- src/graphql/loaders/getMyInfoLoader.js
+++ src/graphql/loaders/getMyInfoLoader.js
@@ -1,7 +1,10 @@
 import DataLoader from 'dataloader';
+import { returnUsers } from '../../database/utils';
 
 export const getMyInfoLoader = ({ User }) =>
   new DataLoader(async userIds => {
     const users = await User.find({ _id: { $in: userIds } });
-    return userIds.map(userId => users.find(({ id }) => userId === id));
+    return userIds.map(userId =>
+      returnUsers(users.find(({ id }) => userId === id)),
+    );
   });
