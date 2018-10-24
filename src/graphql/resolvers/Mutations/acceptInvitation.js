74ada8ea3b5100f50e752b8d933305878e616ffa
Brian Andrews <sbardian@gmail.com>
2018-10-24 08:34:48 -0600

    Update accept invitation to work with new invitee object

--- src/graphql/resolvers/Mutations/acceptInvitation.js
+++ src/graphql/resolvers/Mutations/acceptInvitation.js
@@ -6,15 +6,17 @@ export const acceptInvitation = async (
   { models: { Invitation, List, User }, user },
 ) => {
   const invitation = await Invitation.findById(invitationId);
-  if (invitation.invitee === user.id || user.isAdmin) {
+  if (invitation.invitee.id === user.id || user.isAdmin) {
     const [{ lists }, { users, id }] = await Promise.all([
-      User.findById(invitation.invitee),
+      User.findById(invitation.invitee.id),
       List.findById(invitation.list),
     ]);
     await User.findByIdAndUpdate(user.id, {
       lists: [...lists, invitation.list],
     });
-    await List.findByIdAndUpdate(id, { users: [...users, invitation.invitee] });
+    await List.findByIdAndUpdate(id, {
+      users: [...users, invitation.invitee.id],
+    });
     await Invitation.findByIdAndRemove(invitationId);
     return returnInvitations(invitation);
   }
