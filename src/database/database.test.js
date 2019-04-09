f722fa3109acd50afdf173c2faa2a381d86e8971
Brian Andrews <sbardian@gmail.com>
2019-04-09 14:46:04 -0600

    Close mongodb connection after database test

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -4,5 +4,6 @@ describe('Database connect tests', () => {
   it('Successfully connects to the database', async () => {
     const mongoose = await connectDB();
     expect(mongoose.connection.readyState).toEqual(1);
+    mongoose.connection.close();
   });
 });
