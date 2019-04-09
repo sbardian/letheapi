9b37d9660f8026b4c1cc0da878653123e44f9026
Brian Andrews <sbardian@gmail.com>
2019-04-09 13:56:57 -0600

    Add database connection tests

---
+++ src/database/database.test.js
@@ -0,0 +1,8 @@
+import connectDB from './database';
+
+describe('Database connect tests', () => {
+  it('Successfully connects to the database', async () => {
+    const mongoose = await connectDB();
+    expect(mongoose.connection.readyState).toEqual(1);
+  });
+});
