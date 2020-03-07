ea4a8c44c11f6a7e96d9c891f60022fd9b874519
Brian Andrews <sbardian@gmail.com>
2020-03-06 20:35:05 -0700

    chore: add port env var

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { createServer } from './server';
 
 (async () => {
   const { engine, httpServer } = await createServer();
-  engine.listen({ port: 9999, httpServer });
+  engine.listen({ port: process.env.PORT, httpServer });
 })();
