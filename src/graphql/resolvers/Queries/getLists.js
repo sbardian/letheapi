b8db3cd43b6e16069bb20b71fa100d6da9d1ebb7
Brian Andrews <sbardian@gmail.com>
2021-01-26 21:35:13 -0700

    refactor: check for user

--- src/graphql/resolvers/Queries/getLists.js
+++ src/graphql/resolvers/Queries/getLists.js
@@ -15,7 +15,7 @@ export const getLists = async (
     throw new AuthenticationError('Invalid token');
   }
   let lists;
-  if (user.isAdmin) {
+  if (user?.isAdmin) {
     lists = await List.find({
       ...(userId && { users: userId }),
       ...(contains_title && {
@@ -27,17 +27,19 @@ export const getLists = async (
     }).limit(limit);
     return lists.map((list) => getListsLoader.load(list.id));
   }
-  if (getOnlySelf(user, userId) || !userId) {
-    lists = await List.find({
-      users: user.id,
-      ...(contains_title && {
-        title: { $regex: `${contains_title}`, $options: 'i' },
-      }),
-      ...(id_is && {
-        _id: id_is,
-      }),
-    }).limit(limit);
-    return lists.map((list) => getListsLoader.load(list.id));
+  if (user) {
+    if (getOnlySelf(user, userId) || !userId) {
+      lists = await List.find({
+        users: user.id,
+        ...(contains_title && {
+          title: { $regex: `${contains_title}`, $options: 'i' },
+        }),
+        ...(id_is && {
+          _id: id_is,
+        }),
+      }).limit(limit);
+      return lists.map((list) => getListsLoader.load(list.id));
+    }
   }
   throw new Error('You are only allowed to retrieve your own lists');
 };
