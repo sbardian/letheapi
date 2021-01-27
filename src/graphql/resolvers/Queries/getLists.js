a9c33c941454e4497aab3cd8ba02407e7037b61d
Brian Andrews <sbardian@gmail.com>
2021-01-26 20:24:14 -0700

    refactor: add user conditional

--- src/graphql/resolvers/Queries/getLists.js
+++ src/graphql/resolvers/Queries/getLists.js
@@ -14,32 +14,30 @@ export const getLists = async (
   if (!(await isTokenValid(token, BlacklistedToken))) {
     throw new AuthenticationError('Invalid token');
   }
-  if (user) {
-    let lists;
-    if (user.isAdmin) {
-      lists = await List.find({
-        ...(userId && { users: userId }),
-        ...(contains_title && {
-          title: { $regex: `${contains_title}`, $options: 'i' },
-        }),
-        ...(id_is && {
-          _id: id_is,
-        }),
-      }).limit(limit);
-      return lists.map((list) => getListsLoader.load(list.id));
-    }
-    if (getOnlySelf(user, userId) || !userId) {
-      lists = await List.find({
-        users: user.id,
-        ...(contains_title && {
-          title: { $regex: `${contains_title}`, $options: 'i' },
-        }),
-        ...(id_is && {
-          _id: id_is,
-        }),
-      }).limit(limit);
-      return lists.map((list) => getListsLoader.load(list.id));
-    }
+  let lists;
+  if (user.isAdmin) {
+    lists = await List.find({
+      ...(userId && { users: userId }),
+      ...(contains_title && {
+        title: { $regex: `${contains_title}`, $options: 'i' },
+      }),
+      ...(id_is && {
+        _id: id_is,
+      }),
+    }).limit(limit);
+    return lists.map((list) => getListsLoader.load(list.id));
+  }
+  if (getOnlySelf(user, userId) || !userId) {
+    lists = await List.find({
+      users: user.id,
+      ...(contains_title && {
+        title: { $regex: `${contains_title}`, $options: 'i' },
+      }),
+      ...(id_is && {
+        _id: id_is,
+      }),
+    }).limit(limit);
+    return lists.map((list) => getListsLoader.load(list.id));
   }
   throw new Error('You are only allowed to retrieve your own lists');
 };
