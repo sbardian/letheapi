067348e8843950931bbb9cb4a32dd2439144b684
Brian Andrews <sbardian@gmail.com>
2021-02-15 21:35:10 -0700

    refactor: remove todo

--- src/graphql/loaders/getUserLoader.js
+++ src/graphql/loaders/getUserLoader.js
@@ -1,8 +1,7 @@
 import DataLoader from 'dataloader';
 
-// TODO: limit return to just a user top level data, no edge data. ?
 export const getUserLoader = ({ User }) =>
-  new DataLoader(async userIds => {
+  new DataLoader(async (userIds) => {
     const users = await User.find({ _id: { $in: userIds } });
-    return userIds.map(userId => users.find(({ id }) => userId === id));
+    return userIds.map((userId) => users.find(({ id }) => userId === id));
   });
